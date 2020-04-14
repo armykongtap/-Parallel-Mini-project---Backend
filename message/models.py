a0c22da9ed30433116e8eef1935c68b6855c758e
David <d19450t@gmail.com>
2020-04-15 00:22:26 +0700

    Update Message model

--- message/models.py
+++ message/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
-class Message(models.Model):
-    msgId = models.AutoField(primary_key=True)
+class Message(models.Model) :
+    msgID = models.AutoField(primary_key=True)
     msg = models.CharField(max_length=100)
+    sender = models.ForeignKey(User, related_name='message_sender', on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, related_name='group', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) :
+        return self.msg
+
+    class Meta :
+        ordering = ('timestamp',)
+
+# Message - msgID, msg, senderID, groupID, timestamp
+# Group - groupID, userID
+# User - userID, groupID, recentMessage
