61c5cffe3484d8e355e729a2632b08ac65fb840e
David <d19450t@gmail.com>
2020-04-15 16:10:55 +0700

    Add URLs for APIs

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
+    path('api/', include('api.urls')),
 ]
