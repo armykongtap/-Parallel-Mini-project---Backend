86108604a978b5502fae4d0842a189e4a5bc23fa
armykongtap <35949144+armykongtap@users.noreply.github.com>
2020-04-19 19:31:59 +0700

    Update consumers.py

--- chat/consumers.py
+++ chat/consumers.py
@@ -8,6 +8,19 @@ from message.models import Message
 
 
 class ChatConsumer(WebsocketConsumer):
+    def send_MessageModel_to_group(self, m):
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'msg_text': m.msg_text,
+                'user_id': m.msg_sender.user_id,
+                'time_stamp': m.msg_timestamp.timestamp()
+            }
+        )
+
+    # -------------------------------------------------------------
+
     def connect(self):
         self.group_id = self.scope['url_route']['kwargs']['group_id']
         self.room_group_name = 'chat_%s' % self.group_id
@@ -20,15 +33,10 @@ class ChatConsumer(WebsocketConsumer):
 
         self.accept()
 
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': 'connected',
-                'user_id': 0,
-                # 'group_id': 0,
-            }
-        )
+        intMessage = Message.objects.filter(msg_group_id=self.group_id)
+        print(type(intMessage))
+        for m in intMessage[Message.objects.count()-10:]:
+            self.send_MessageModel_to_group(m)
 
     def disconnect(self, close_code):
         # Leave room group
@@ -38,32 +46,27 @@ class ChatConsumer(WebsocketConsumer):
         )
 
     # Receive message from WebSocket
-    # @sync_to_async
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        msg_text = text_data_json['msg_text']
         user_id = text_data_json['user_id']
 
-        Message(msg_text=message, msg_sender=User.objects.get(
-            pk=user_id), msg_group=Group.objects.get(pk=self.group_id)).save()
+        m = Message(msg_text=msg_text, msg_sender=User.objects.get(
+            pk=user_id), msg_group=Group.objects.get(pk=self.group_id))
+        m.save()
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-                'user_id': user_id,
-            }
-        )
+        self.send_MessageModel_to_group(m)
 
     # Receive message from room group
     def chat_message(self, event):
-        message = event['message']
+        msg_text = event['msg_text']
         user_id = event['user_id']
+        time_stamp = event['time_stamp']
 
         # Send message to WebSocket
         self.send(text_data=json.dumps({
-            'message': message,
+            'msg_text': msg_text,
             'user_id': user_id,
+            'time_stamp': time_stamp,
         }))
