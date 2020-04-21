b9b54257f50e79eb36d7c41fcb4df334e97e68f5
armykongtap <35949144+armykongtap@users.noreply.github.com>
2020-04-21 15:54:29 +0700

    Update consemers and router
    can handel recent message

--- chat/consumers.py
+++ chat/consumers.py
@@ -10,6 +10,8 @@ from message.models import Message
 
 
 class ChatConsumer(WebsocketConsumer):
+    online_user = set()
+
     def message_to_dict(self, m):
         return({
             'type': 'chat_message',
@@ -19,29 +21,62 @@ class ChatConsumer(WebsocketConsumer):
             'time_stamp': timezone('Asia/Bangkok').localize(m.msg_timestamp).strftime("%H:%M")
         })
 
+    def update_recent_message(self, msg_id):
+        user = User.objects.get(user_name=self.user_name)
+        newmsg = Message.objects.get(msg_id=msg_id)
+        getmsg = user.user_recent_message.filter(msg_group=self.group_id)
+        if newmsg.msg_group.group_id == self.group_id:
+            if getmsg.exists():
+                rm = user.user_recent_message.get(msg_group=self.group_id)
+                if msg_id > rm.msg_id:
+                    user.user_recent_message.remove(rm)
+                    user.user_recent_message.add(newmsg)
+            else:
+                user.user_recent_message.add(newmsg)
+
+    def get_recent_message(self):
+        query = {}
+        query['user_name'] = self.user_name
+        query['group_id'] = self.group_id
+        getmsg = User.objects.get(
+            user_name=self.user_name).user_recent_message.filter(msg_group=self.group_id)
+        if getmsg.exists():
+            query['recent_msg_id'] = getmsg[0].msg_id
+        else:
+            query['recent_msg_id'] = 0
+        return query
+
     # -------------------------------------------------------------
 
     def connect(self):
-        self.group_id = self.scope['url_route']['kwargs']['group_id']
+        self.group_id = int(self.scope['url_route']['kwargs']['group_id'])
         self.room_group_name = 'chat_%s' % self.group_id
-
+        self.user_name = self.scope['url_route']['kwargs']['user_name']
+        self.online_user.add(self.user_name)
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
-
         self.accept()
 
+        self.send(text_data=json.dumps(
+            self.get_recent_message()))
+
         initMessage = Message.objects.filter(msg_group_id=self.group_id)
-        if (Message.objects.count() >= 100):
-            n = Message.objects.count()-100
+        if (initMessage.count() > 10):
+            n = initMessage.count()-10
         else:
             n = 0
         for m in initMessage[n:]:
             self.send(text_data=json.dumps(self.message_to_dict(m)))
+        if initMessage.count() > 0:
+            self.update_recent_message(
+                initMessage[initMessage.count()-1].msg_id)
 
     def disconnect(self, close_code):
+        self.online_user.discard(
+            self.user_name)
         # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
@@ -50,19 +85,19 @@ class ChatConsumer(WebsocketConsumer):
 
     # Receive message from WebSocket
     def receive(self, text_data):
+
         text_data_json = json.loads(text_data)
         msg_text = text_data_json['msg_text']
-        user_name = text_data_json['user_name']
 
         m = Message(msg_text=msg_text, msg_sender=User.objects.get(
-            user_name=user_name), msg_group=Group.objects.get(pk=self.group_id))
+            user_name=self.user_name), msg_group=Group.objects.get(pk=self.group_id))
         m.save()
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name, self.message_to_dict(m))
-    # Receive message from room group
 
+    # Receive message from room group
     def chat_message(self, event):
         msg_id = event['msg_id']
         msg_text = event['msg_text']
@@ -76,3 +111,5 @@ class ChatConsumer(WebsocketConsumer):
             'user_name': user_name,
             'time_stamp': time_stamp,
         }))
+
+        self.update_recent_message(msg_id)
--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<group_id>\w+)/$', consumers.ChatConsumer),
+    re_path(r'ws/chat/user_name=(?P<user_name>\w+)&group_id=(?P<group_id>\w+)/$',
+            consumers.ChatConsumer),
 ]
